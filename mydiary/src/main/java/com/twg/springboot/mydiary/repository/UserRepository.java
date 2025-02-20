f9f6b21c07aab33e162853bee31ef9e0246f2d2c
MADHUBABU <madhubabu12345abcde@gmail.com>
2025-02-20 13:23:29 +0530

    Add files via upload

---
+++ mydiary/src/main/java/com/twg/springboot/mydiary/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.twg.springboot.mydiary.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.twg.springboot.mydiary.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	@Query(value = "select * from users where username =:username LIMIT 1 ",nativeQuery = true)
+	public User findByUsername(String username);
+	
+}
